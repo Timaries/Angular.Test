84520cc2826a397d640f7b21fa300b9e94ea3de5
Temo Kakushadze <kakushadzet@gmail.com>
2022-07-04 00:31:03 +0400

    New

---
+++ src/app/customer/customer.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomergridComponent } from './customergrid/customergrid.component';
+
+
+
+@NgModule({
+  declarations: [
+    CustomergridComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CustomerModule { }
